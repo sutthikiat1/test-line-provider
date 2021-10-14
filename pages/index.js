a47807bfa2ec5e4c051c8a3638b5d60b9b2b3cc7
sutthikiat <sutthikiat.ph@gmail.con>
2021-10-14 17:49:07 +0700

    redirect url

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,19 @@
-import Head from "next/head";
 import { useEffect, useState } from "react";
 const KEY_LINE_LIFF = "1656533574-jOJ3rRv4";
-
+import { useRouter } from "next/router";
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const queryString = decodeURIComponent(window.location.search).replace(
+      "?liff.state=",
+      ""
+    );
+    console.log("url", queryString);
+    if (queryString) {
+      router.push(`${queryString}`);
+    }
+  }, []);
+
   return <div className="container">Home</div>;
 }
