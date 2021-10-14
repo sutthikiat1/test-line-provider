6c8bdb9b43a39a1cb9901c579e38e8b7b73fb618
sutthikiat <sutthikiat.ph@gmail.con>
2021-10-14 18:41:05 +0700

    Fix

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,20 @@
 import { useEffect } from "react";
+const KEY_LINE_LIFF = "1656533574-jOJ3rRv4";
+
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    new window.VConsole();
+    (async () => {
+      await initialLineLiff();
+      new window.VConsole();
+    })();
   }, []);
+
+  const initialLineLiff = async () => {
+    await liff
+      .init({ liffId: KEY_LINE_LIFF })
+      .then(() => console.log(":: Liff init ::"));
+  };
+
   return <Component {...pageProps} />;
 }
 
