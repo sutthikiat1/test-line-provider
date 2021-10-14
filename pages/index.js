bd81db64afa50dba17ed255d4b11de4ac55bee77
sutthikiat <sutthikiat.ph@gmail.con>
2021-10-14 18:37:59 +0700

    check

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+const KEY_LINE_LIFF = "1656533574-jOJ3rRv4";
 export default function Home() {
   const router = useRouter();
   const [useLocaiton, setLocation] = useState("");
   useEffect(() => {
     setLocation(window.location.href);
   });
+
+  useEffect(() => {
+    (async () => {
+      await initialLineLiff();
+    })();
+  }, []);
+
+  const initialLineLiff = async () => {
+    await liff
+      .init({ liffId: KEY_LINE_LIFF })
+      .then(() => console.log(":: Liff init ::"));
+  };
 
   return <div className="container">Home {useLocaiton}</div>;
 }
