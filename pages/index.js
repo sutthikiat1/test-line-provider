a5488f29f2c346a58fe2c375b0440e1fb7517c87
sutthikiat <sutthikiat.ph@gmail.con>
2021-10-14 18:09:06 +0700

    test2

--- pages/index.js
+++ pages/index.js
@@ -3,22 +3,6 @@ import { useRouter } from "next/router";
 export default function Home() {
   const router = useRouter();
   const [useLocaiton, setLocation] = useState("");
-  useEffect(() => {
-    setLocation(window.location.href);
-    console.log(":: before ::");
-    const queryString = decodeURIComponent(window.location.search).replace(
-      "?liff.state=",
-      ""
-    );
-    console.log("url", queryString);
-    if (queryString) {
-      router.push(`${queryString}`);
-    }
-  });
-
-  // useEffect(() => {
-
-  // }, []);
 
   return <div className="container">Home {useLocaiton}</div>;
 }
