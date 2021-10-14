4fd5a32bd592a2495d3861ba073900d2bcd9560d
sutthikiat <sutthikiat.ph@gmail.con>
2021-10-14 18:11:59 +0700

    tt

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,12 @@ import { useRouter } from "next/router";
 export default function Home() {
   const router = useRouter();
   const [useLocaiton, setLocation] = useState("");
+  useEffect(() => {
+    setLocation(window.location.href);
+    if (!liff.isLoggedIn()) {
+      liff.login({ redirectUri: `${window.location.href}/abc` });
+    }
+  });
 
   return <div className="container">Home {useLocaiton}</div>;
 }
